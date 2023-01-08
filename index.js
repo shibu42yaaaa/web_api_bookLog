4a647d27d4565781e5e8f0180c0605336ba74a92
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-08 17:06:58 +0900

    postmanでのテスト完了

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.json())
+
+app.post('/booklog', (req, res) => {
+    res.json({
+        "ok": true
+    })
+})
 
 app.listen(port, () => {
     console.log('App Listening At http://localhost:${port}')
